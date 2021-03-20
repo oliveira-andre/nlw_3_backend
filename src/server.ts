fc51ebd7506a2d65181fa0832a478d2d134da04c
Andre Oliveira <andreoliveirar2d2@gmail.com>
2021-03-20 02:26:26 -0300

    feat: routing on separated file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
-import './database/connection';
+import './database/conection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  return res.json({ ok: true });
-});
+app.use(routes);
 
 app.listen(3333);
