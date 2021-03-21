f9ec9fdb766a193f828ececf18a591af2138cd83
Andre Oliveira <andreoliveirar2d2@gmail.com>
2021-03-21 01:34:32 -0300

    feat: applying to post route the middleware config images

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
-routes.get('/orphanages/:id', OrphanagesController.show);
+routes.get('/orphanages/:id', upload.array('images'), OrphanagesController.show);
 
 export default routes;
