8186e75cc73623b283787e348593dbea45f57c0c
Andre Oliveira <andreoliveirar2d2@gmail.com>
2021-03-21 00:58:52 -0300

    feat: routing to show orphanage

--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,6 @@ const routes = Router();
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages/:id', OrphanagesController.show);
 
 export default routes;
