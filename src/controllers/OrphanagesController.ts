5b2204676bd0f04f82fb059c262c4f025ddd3e04
Andre Oliveira <andreoliveirar2d2@gmail.com>
2021-03-22 23:42:41 -0300

    feat: rendering views on controller

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,13 +2,16 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
 import Orphanage from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
 
 export default {
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
 
-    return res.json(orphanages);
+    return res.json(orphanageView.renderMany(orphanages));
   },
 
   async create(req: Request, res: Response) {
@@ -47,8 +50,10 @@ export default {
   async show(req: Request, res: Response) {
     const { id } = req.params;
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.findOneOrFail(id);
+    const orphanages = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
-    return res.json(orphanages);
+    return res.json(orphanageView.render(orphanages));
   }
 }
